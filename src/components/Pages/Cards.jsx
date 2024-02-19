def4401082142cd131191202309d544cdb3b5b86
Olayemi <ogunrinde_olayemi@yahoo.com>
2024-02-19 13:19:31 +0100

    new update

--- src/components/Pages/Cards.jsx
+++ src/components/Pages/Cards.jsx
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom';
 
 const ServiceCard = ({ imageUrl, title, description, link }) => {
   return (
-    <Link to={link} className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 rounded overflow-hidden mx-4 my-8 transform hover:scale-105 transition duration-300 ease-in-out">
+    <Link to={link} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 rounded overflow-hidden mx-4 my-8 transform hover:scale-105 transition duration-300 ease-in-out">
       <img className="w-full h-auto" src={imageUrl} alt={title} />
       <div className=" bg-indigo-800 px-6 py-4">
-        <div className="font-bold text-white text-3xl mb-2">{title}</div>
-        <p className="text-white text-base">{description}</p>
+        <div className="font-bold text-white text-lg mb-2">{title}</div>
+        <p className="text-white text-lg">{description}</p>
       </div>
     </Link>
   );
 };
+
 
 const Cards = () => {
   return (
@@ -33,7 +34,7 @@ const Cards = () => {
         />
         <ServiceCard
           imageUrl="https://img.freepik.com/free-photo/teacher-putting-her-hands-together-with-her-students-game_23-2148633397.jpg?size=626&ext=jpg&ga=GA1.2.391507465.1706553752&semt=ais"
-          title="Intervention and Support Services"
+          title="Intervention  Services"
           
           link="/service2" 
         />
@@ -45,13 +46,13 @@ const Cards = () => {
         />
         <ServiceCard
           imageUrl="https://img.freepik.com/premium-photo/family-staying-hotel_53876-48206.jpg?size=626&ext=jpg&ga=GA1.2.391507465.1706553752&semt=ais"
-          title="Safe Housing and Community Support"
+          title="Safe Housing and Community "
           
           link="/service4" 
         />
         <ServiceCard
           imageUrl="https://img.freepik.com/free-photo/view-young-children-suffering-from-abuse_23-2150248246.jpg?size=626&ext=jpg&ga=GA1.2.391507465.1706553752&semt=ais"
-          title="Collaborative Multi-Agency Approach"
+          title=" Multi-Agency Approach"
           
           link="/service3" 
         />
