3f35c2d4b9f4000aa369e190d27793f6c85b3d08
Olayemi <ogunrinde_olayemi@yahoo.com>
2024-02-20 19:52:04 +0100

    Middle

--- src/components/Pages/WhoWeAre.jsx
+++ src/components/Pages/WhoWeAre.jsx
@@ -16,7 +16,7 @@ const WhoWeAre = () => {
           <iframe
             width="600"
             height="315"
-            src="https://www.youtube.com/embed/VIDEO_ID_HERE"
+            src="https://www.youtube.com/embed/VIDEO_ID"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
