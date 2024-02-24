8b4277b1991cfb6866e64ca9be20e323b8370c28
Olayemi <ogunrinde_olayemi@yahoo.com>
2024-02-24 18:38:22 +0100

    Offling

---
+++ src/components/Pages/AboutUsPage.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import MainLayouts from '../Layouts/MainLayouts';
+
+const AboutUsPage = () => {
+  return (
+    <MainLayouts>
+      <div className='mt-56 relative overflow-hidden'>
+        <img
+          src="https://media.istockphoto.com/id/466292648/photo/metal-about-us-text.jpg?s=612x612&w=0&k=20&c=H1Hu-TrG4m1nbG6lKTo_lz11M925RkR9ofZnJAnc1Pw="
+          alt="About Us Banner"
+          className="w-full object-cover"
+          style={{ height: '550px', minWidth: '100vw', position: 'relative', left: '50%', transform: 'translateX(-50%)'}}
+        />
+        <div className="container mx-auto px-4 mt-2">
+          <div className="max-w-3xl mx-auto leading-relaxed">
+            <h2 className='text-2xl text-blue-950 font-semibold mb-2 text-center'>Who We Are</h2>
+            <p className="mb-4 text-xl">
+                We are a team of experienced professionals with a passion for making a positive difference in 
+                the lives of children in residential care settings. Our consultants bring together a wealth of 
+                knowledge and expertise in child welfare, regulatory compliance, leadership development, and training.
+            </p>
+            <h2 className='text-2xl text-blue-950 font-semibold mt-4 mb-4 text-center'>Our Mission</h2>
+            <p className="mb-4 text-xl">
+                Our mission is clear: to ensure that every child in residential care receives the exceptional support they deserve. 
+                Through our consultancy services, we provide guidance and assistance to care homes,
+                enabling them to meet regulatory requirements while prioritizing the welfare of the children they serve.
+            </p>
+            <h2 className='text-2xl text-blue-950 font-semibold mt-4 mb-4 text-center'>Our Mission</h2>
+            <p className="mb-4 text-xl">
+                We envision a future where every child in residential care thrives in a nurturing environment. By working closely with care homes, 
+                we aim to optimize their operations, ensuring peak efficiency, effectiveness, and compliance. 
+                Our vision is one of positive outcomes and experiences for all involved, with children receiving 
+                the care and support they need to flourish.
+            </p>
+          </div>
+        </div>
+      </div>
+    </MainLayouts>
+  );
+};
+
+export default AboutUsPage;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import AboutUsPage from './components/Pages/AboutUsPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
-      {/* <Route path="/AboutUs" element={<AboutUs />} />
-      <Route path="/Services" element={<Services />} />
+      <Route path="/AboutUs" element={<AboutUsPage />} />
+      {/* <Route path="/Services" element={<Services />} />
       <Route path="/Contact" element={<ContactUs /> } />       */}
     </Routes>
   </Router>
