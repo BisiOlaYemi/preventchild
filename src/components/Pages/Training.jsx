b8cbaf1c1a3a9892c48859025cda450f8d9c9e6c
Olayemi <ogunrinde_olayemi@yahoo.com>
2024-02-27 08:14:01 +0100

    Linking

--- src/components/Pages/Training.jsx
+++ src/components/Pages/Training.jsx
@@ -17,13 +17,16 @@ const Training = () => {
         <div className="container mx-auto px-4 mt-8">
             <h2 className='text-2xl mt-8 text-blue-950 font-bold'>Training</h2>
             <p className="text-xl font-[Open Sans] mb-2 mt-4" >We offer a wide range of health and social care courses, these range from a few hours up to 10 days over a few months. All of our courses are:</p>
-            <ul className="text-lg font-[Open Sans] mb-2 mt-2">
+            <ul className="list-disc text-lg font-[Open Sans] mb-2 mt-2">
                 <li>Based on current legislation and best practice</li>
                 <li>Delivered by a trainer with extensive experience of the subject</li>
                 <li>Engaging, fun and thought provoking</li>
                 <li>Designed to; improve knowledge, increase confidence, develop competence</li>
                 <li>Delivered by a trainer with a proper training qualification e.g. PGCE</li>
             </ul>
+        
+          <p className="text-lg font-[Open Sans] mb-2 mt-4">We are able to deliver both face to face and virtual courses.  Face to face courses take into account all
+            current health and safety requirements. Virtual courses are delivered via Zoom.</p>
             
             <div className="flex flex-wrap justify-center">
 
