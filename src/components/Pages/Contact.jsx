2087642d6d70ea46f697d85e3556ee6a3e134fc3
Olayemi <ogunrinde_olayemi@yahoo.com>
2024-05-06 11:15:05 +0100

    changing the images background

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import MainLayout from '../Layouts/MainLayouts';
-import CONTACTUS from '../../assets/CONTACTUS.jpg'
+import CONTACTUS from '../../assets/CONTACTUS.jpg';
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -19,7 +19,12 @@ const Contact = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
+    if (parseInt(formData.humanVerification) !== 4) {
+      alert('Please solve the human verification Sum correctly.');
+      return;
+    }
+
     try {
       const response = await fetch('https://getform.io/f/jawljzqb', {
         method: 'POST',
@@ -28,109 +33,105 @@ const Contact = () => {
         },
         body: JSON.stringify(formData),
       });
-  
+
       if (response.ok) {
-        
         alert('Form submitted successfully!');
       } else {
-        
         alert('Failed to submit form. Please try again later.');
       }
     } catch (error) {
-      
       console.error('Error submitting form:', error);
       alert('Failed to submit form. Please try again later.');
     }
   };
-  
 
   return (
     <MainLayout>
-    <div className='mt-56 relative overflow-hidden'>
+      <div className='mt-56 relative overflow-hidden'>
         <img
-            src={CONTACTUS}
-            alt="Contact Us Banner"
-            className="w-full object-cover"
-            style={{ height: '550px', minWidth: '100vw', position: 'relative', left: '50%', transform: 'translateX(-50%)'}}
+          src={CONTACTUS}
+          alt="Contact Us Banner"
+          className="w-full object-cover"
+          style={{ height: '550px', minWidth: '100vw', position: 'relative', left: '50%', transform: 'translateX(-50%)'}}
         />
-         <div className='mt-10 sm:mt-24'>
-        <h2 className='text-xl sm:text-2xl font-[Open Sans] mt-4 sm:mt-8 text-center sm:text-left pl-4 sm:pl-16'>Please complete Enquiry Form below</h2>
-        <div className="flex flex-col sm:flex-row mt-4 sm:mt-8 pl-4 sm:pl-16">
-          <form className="w-full sm:w-1/2 p-4">
-            <input
-              type="text"
-              name="name"
-              placeholder="Enter your Name"
-              value={formData.name}
-              onChange={handleChange}
-              className="w-full mb-4 p-2 border rounded"
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full mb-4 p-2 border rounded"
-            />
-            <input
-              type="tel"
-              name="phone"
-              placeholder="Phone"
-              value={formData.phone}
-              onChange={handleChange}
-              className="w-80 mb-4 p-2 border rounded"
-            />
-            <select
-              name="subject"
-              value={formData.subject}
-              onChange={handleChange}
-              className="w-80 mb-4 p-2 border rounded"
-            >
-              <option value="General Enquiry">General Enquiry</option>
-              <option value="Services">Services</option>
-              <option value="Training">Training</option>
-            </select>
-            <textarea
-              name="comments"
-              placeholder="Comments"
-              value={formData.comments}
-              onChange={handleChange}
-              className="w-full mb-4 p-2 border rounded"
-            />
-            <div className="flex items-center mb-4">
-              <label htmlFor="humanVerification" className="mr-2 font-[Open Sans] text-lg">
-                Are you human?
-              </label>
-              <span className="ml-2 pr-2 font-[Open Sans] text-lg">3 + 1 = </span>
+        <div className='mt-10 sm:mt-24'>
+          <h2 className='text-xl sm:text-2xl font-[Open Sans] mt-4 sm:mt-8 text-center sm:text-left pl-4 sm:pl-16'>Please complete Enquiry Form below</h2>
+          <div className="flex flex-col sm:flex-row mt-4 sm:mt-8 pl-4 sm:pl-16">
+            <form className="w-full sm:w-1/2 p-4">
               <input
                 type="text"
-                name="humanVerification"
-                value={formData.humanVerification}
+                name="name"
+                placeholder="Enter your Name"
+                value={formData.name}
                 onChange={handleChange}
-                className="w-16 p-2 border rounded"
+                className="w-full mb-4 p-2 border rounded"
               />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full mb-4 p-2 border rounded"
+              />
+              <input
+                type="tel"
+                name="phone"
+                placeholder="Phone"
+                value={formData.phone}
+                onChange={handleChange}
+                className="w-80 mb-4 p-2 border rounded"
+              />
+              <select
+                name="subject"
+                value={formData.subject}
+                onChange={handleChange}
+                className="w-80 mb-4 p-2 border rounded"
+              >
+                <option value="General Enquiry">General Enquiry</option>
+                <option value="Services">Services</option>
+                <option value="Training">Training</option>
+              </select>
+              <textarea
+                name="comments"
+                placeholder="Comments"
+                value={formData.comments}
+                onChange={handleChange}
+                className="w-full mb-4 p-2 border rounded"
+              />
+              <div className="flex items-center mb-4">
+                <label htmlFor="humanVerification" className="mr-2 font-[Open Sans] text-lg">
+                  Are you human?
+                </label>
+                <span className="ml-2 pr-2 font-[Open Sans] text-lg">3 + 1 = </span>
+                <input
+                  type="text"
+                  name="humanVerification"
+                  value={formData.humanVerification}
+                  onChange={handleChange}
+                  className="w-16 p-2 border rounded"
+                />
+              </div>
+              <button
+                type="submit"
+                onClick={handleSubmit}
+                className="bg-blue-950 text-white px-4 py-2 rounded w-40"
+              >
+                Submit
+              </button>
+            </form>
+            <div className="w-full sm:w-1/2 p-4 text-center sm:text-left font-[Open Sans] text-lg text-center">
+              <p className="font-bold">Contact details</p>
+              <p>Telephone: 07414899199 </p>
+              <p>Email: info@preventchildrensocialcare.co.uk</p>
+              <p>Website: www.preventchildrensocialcare.co.uk</p>
+              <p className="font-bold mt-4">Address</p>
+              <p>Prevent Children Social Care Consultancy</p>
+              <p>214, High Street, Herne Bay, Kent CT6 5AX</p>
             </div>
-            <button
-              type="submit"
-              onClick={handleSubmit}
-              className="bg-blue-950 text-white px-4 py-2 rounded w-40"
-            >
-              Submit
-            </button>
-          </form>
-          <div className="w-full sm:w-1/2 p-4 text-center sm:text-left font-[Open Sans] text-lg text-center">
-            <p className="font-bold">Contact details</p>
-            <p>Telephone: 07414899199 </p>
-            <p>Email: info@preventchildrensocialcare.co.uk</p>
-            <p>Website: www.preventchildrensocialcare.co.uk</p>
-            <p className="font-bold mt-4">Address</p>
-            <p>Prevent Children Social Care Consultancy</p>
-            <p>214, High Street, Herne Bay, Kent CT6 5AX</p>
           </div>
         </div>
       </div>
-    </div>
     </MainLayout>
   );
 };
